999b521be54f01a6f8eb24820a50e68f3a68daf3
Norbi96 <pavelkanorbert@gmail.com>
2023-04-17 16:21:42 +0200

    chn

--- src/Menu/ProductDetails/EditProduct.js
+++ src/Menu/ProductDetails/EditProduct.js
@@ -5,40 +5,40 @@ import '../../css/product.css';
 
 export default function EditProduct() {
 
-  // const params = useParams();
+  const params = useParams();
 
-  // const { products, setProducts } = useContext(productContext);
+  const { products, setProducts } = useContext(productContext);
 
-  // const find = products.filter(prod => prod.id === params.id && prod)
+  const find = products.filter(prod => prod.id === params.id && prod)
 
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
-  // const [name, setName] = useState(find[0].name)
-  // const [price, setPrice] = useState(find[0].price)
-  // const [amount, setAmount] = useState(find[0].quantity)
-  // const [img, setImg] = useState(find[0].imgsrc)
+  const [name, setName] = useState(find[0].name)
+  const [price, setPrice] = useState(find[0].price)
+  const [amount, setAmount] = useState(find[0].quantity)
+  const [img, setImg] = useState(find[0].imgsrc)
 
-  // function editProductHandler() {
+  function editProductHandler() {
 
-  // const idx = products.indexOf(find[0])
+    const idx = products.indexOf(find[0])
 
-  // const update = {
-  //   ...find[0],
-  //   name: name,
-  //   price: Number(price),
-  //   quantity: Number(amount),
-  //   imgsrc: img
-  // }
+    const update = {
+      ...find[0],
+      name: name,
+      price: Number(price),
+      quantity: Number(amount),
+      imgsrc: img
+    }
 
-  // setProducts([
-  //   ...products.slice(0, idx),
-  //   update,
-  //   ...products.slice(idx + 1)
-  // ]
-  // )
+    setProducts([
+      ...products.slice(0, idx),
+      update,
+      ...products.slice(idx + 1)
+    ]
+    )
 
-  //   navigate('/products')
-  // }
+    navigate('/products')
+  }
 
   return (
     <>
@@ -47,30 +47,30 @@ export default function EditProduct() {
         <p>
           <label>
             Product name:
-            <input type='text' />
+            <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
           </label>
         </p>
         <p>
           <label>
             Product price:
-            <input type='text' />
+            <input type='text' value={price} onChange={(e) => setPrice(e.target.value)} />
           </label>
         </p>
         <p>
           <label>
             Product amount:
-            <input type='text' />
+            <input type='text' value={amount} onChange={(e) => setAmount(e.target.value)} />
           </label>
         </p>
         <p>
           <label>
             Product imgsrc:
-            <input type='text' />
+            <input type='text' value={img} onChange={(e) => setImg(e.target.value)} />
           </label>
         </p>
         <div className='product-detail-btn'>
-          {/* <Link to={navigate(-1)}>Back</Link>
-          <button onClick={editProductHandler}>Edit</button> */}
+          <Link to={navigate(-1)}>Back</Link>
+          <button onClick={editProductHandler}>Edit</button>
         </div>
       </div>
     </>

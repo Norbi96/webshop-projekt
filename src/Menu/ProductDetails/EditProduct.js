ac1d501b22a0a83b502a2cc3e1bb3084cce5d41d
Norbi96 <pavelkanorbert@gmail.com>
2023-04-17 18:01:51 +0200

    chn

--- src/Menu/ProductDetails/EditProduct.js
+++ src/Menu/ProductDetails/EditProduct.js
@@ -11,12 +11,15 @@ export default function EditProduct() {
   console.log('id', id);
   const find = products.find(prod => prod.id == id)
 
-
+  let name = find.name
+  let price = find.price
+  let amount = find.quantity
+  let img = find.imgsrc
   const navigate = useNavigate()
-  const [name, setName] = useState(find.name)
-  const [price, setPrice] = useState(find.price)
-  const [amount, setAmount] = useState(find.quantity)
-  const [img, setImg] = useState(find.imgsrc)
+  // const [name, setName] = useState(find.name)
+  // const [price, setPrice] = useState(find.price)
+  // const [amount, setAmount] = useState(find.quantity)
+  // const [img, setImg] = useState(find.imgsrc)
 
   function editProductHandler() {
 
@@ -38,34 +41,47 @@ export default function EditProduct() {
     )
 
     navigate('/products')
+  };
+
+
+
+  function namechange(e) {
+    name = e.target.value
   }
-
-
+  function pricechange(e) {
+    price = e.target.value
+  }
+  function amountchange(e) {
+    amount = e.target.value
+  }
+  function imgehange(e) {
+    img = e.target.value
+  }
   return (
     <>
       <div className='product-detail'>
         <p>
           <label>
             Product name:
-            <input type='text' value={name} />
+            <input type='text' placeholder={name} onChange={namechange} />
           </label>
         </p>
         <p>
           <label>
             Product price:
-            <input type='text' value={price} />
+            <input type='text' placeholder={price} onChange={pricechange} />
           </label>
         </p>
         <p>
           <label>
             Product amount:
-            <input type='text' value={amount} />
+            <input type='text' placeholder={amount} onChange={amountchange} />
           </label>
         </p>
         <p>
           <label>
             Product imgsrc:
-            <input type='text' value={img} />
+            <input type='text' placeholder={img} onChange={imgehange} />
           </label>
         </p>
         <div className='product-detail-btn'>
